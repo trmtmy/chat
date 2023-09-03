9046a425b4baefe669463f79244fef1b31deda2a
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-09-03 14:03:38 +0000

    followview

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
-
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
-    resource :relationships, only: [:create, :destroy, :index]
+    resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
