c05d0c27fcc400df677656366ef70492548a4625
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-09-03 10:38:23 +0000

    follow

---
+++ app/models/user.rb
@@ -0,0 +1,55 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  # フォローした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    # ---リレーション書き方---has_many :xxx, class_name: "モデル名", foreign_key: "○○_id", dependent: :destroy
+  # xxxはアソシエーションが繋がっているテーブル名
+  # class_nameは実際のモデルの名前
+  # foreign_keyは外部キーとして何を持つか
+  has_many :followings,  through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # ---リレーション書き方---has_many :yyy, through: :xxx, source: :zzz
+  # has_many :テーブル名
+  # yyyには架空のテーブル名
+  # through: :中間テーブル名
+  # zzzは実際にデータを取得しにいくテーブル名
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
+  
+  
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: {maximum: 50 }
+
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+
+end
